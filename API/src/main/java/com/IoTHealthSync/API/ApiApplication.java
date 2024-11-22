a7e12ccba52b5a7f73e6d82ed6b2e918cea3fd8d
Steven <stevenpstansberry@gmail.com>
2024-11-21 23:05:00 -0800

    Added middleware

--- API/src/main/java/com/IoTHealthSync/API/ApiApplication.java
+++ API/src/main/java/com/IoTHealthSync/API/ApiApplication.java
@@ -1,10 +1,16 @@
 package com.IoTHealthSync.API;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(ApiApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
---
+++ API/src/main/java/com/IoTHealthSync/API/Config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.IoTHealthSync.API.Config;
+
+import com.IoTHealthSync.API.Middleware.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
